d1a715cc7efc24e7f434997bf5fe72da806ebe29
diegomompo7 <diego.mompo.redoli@gmail.com>
2017-11-07 11:30:57 +0100

    Initial commit

--- 00_prirmitivas/javascript/javascript/main.js
+++ 00_prirmitivas/javascript/javascript/main.js
@@ -1,3 +1,5 @@
-function main () {
-    alert("The world is a vampire")
+function main() {
+    var stdout = document.getElementById("stdout")
+    stdout.innerHTML += "stdout: The world is a vampire"
+    alert("stderr: The world is a vampire")
 }
