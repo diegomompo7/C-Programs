fa03c06e3860d6551903b8b2883201bbfd184eae
diegomompo7 <diego.mompo.redoli@gmail.com>
2018-01-25 11:28:26 +0100

    c-programs

---
+++ 10_arativa/determinante.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main{
+
+// Nombre de la fila n  (div f*n/ c*n//
+// apunto el coeficiente
+// Busco los 0 en los coeficientes n de todas las filas i
+// En la fila i le resto a_n la fila n
+
+    double natriz[N][N] = {
+        {2, 3, 4}
+        {4, 7, 1}
+        {6, 2, 3}
+    
+    };
+
+    double natriz[N][N] = {
+        {0, 0, 0}
+        {0, 0, 0}
+        {0, 0, 0}
+
+    };
+
+    for(int f=0; f<1; f++:) {
+        for(int c=0; c<1; c++;) {
+            natriz[f][c] = f/c;
+        }
+    }
+
+
+
+    return EXIT_SUCCESS;
+}
