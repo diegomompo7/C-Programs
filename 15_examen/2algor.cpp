e8736eca780da22c754b32b84f08dd8fd7769044
diegomompo7 <diego.mompo.redoli@gmail.com>
2018-03-16 13:22:18 +0100

    Correción examen

---
+++ 15_examen/2algor.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 100
+
+bool es_primo(int numero){
+    for(int divisor=2; divisor<numero; divisor++)
+        if(numero%divisor==0)
+            return false;
+    return true;
+}
+int main(){
+
+    int primo[N];
+
+    for(int np = 0, pp=2; np<N; pp++)
+        if(es_primo(pp))
+            primo[np++] = pp;
+
+    for(int f=0; f<N; f++)
+        printf(" %i ", primo[f]);
+
+    printf("\n");
+    return EXIT_SUCCESS;
+}
